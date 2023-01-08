7ac87838e365fce14a1130934620a0a0b8a06382
André Dutra <100073778+AndreDutraDEV@users.noreply.github.com>
2023-01-08 17:25:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 
+let buttonSubmitData = document.getElementById('btn_sbmtClient');
+
+let nameInput = document.getElementById('nomeCliData');
+let emailInput = document.getElementById('emailCliData');
+let foneInput = document.getElementById('foneCliData');
+
+const addLoading = () => {
+    // buttonSubmitData.classList.add('activeLoading');
+    buttonSubmitData.innerHTML = '<span class="spinLoading"></span>'
+    buttonSubmitData.disabled = true;
+}
+
+const removeLoading = () => {
+    // buttonSubmitData.classList.remove('activeLoading');
+    buttonSubmitData.innerHTML = 'Enviar'
+    buttonSubmitData.disabled = false;
+}
+
+const blockinputs = () => {
+
+    nameInput.disabled = true;
+    emailInput.disabled = true;
+    foneInput.disabled = true;
+
+}
+
+const ableinputs = () => {
+
+    nameInput.disabled = false;
+    emailInput.disabled = false;
+    foneInput.disabled = false;
+
+}
 
 const handleSubmit = (event) => {
     event.preventDefault();
 
-    const name = document.getElementById('nomeCliData').value;
-    const email = document.getElementById('emailCliData').value;
-    const fone = document.getElementById('foneCliData').value;
+    let name = document.getElementById('nomeCliData').value;
+    let email = document.getElementById('emailCliData').value;
+    let fone = document.getElementById('foneCliData').value;
 
     const form = document.getElementById('formDataClient');
+
+    blockinputs();
+    addLoading();
+
 
     fetch('https://api.sheetmonkey.io/form/8HKwLubh4LT3NvAgoinQ72', {
 
@@ -17,8 +54,51 @@ const handleSubmit = (event) => {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ Nome: name, Email: email, Telefone: fone})
-    }).then( () => alert('Eviado com Sucesso'))
+        body: JSON.stringify({ Nome: name, Email: email, Telefone: fone })
+    }).then(() => {
+
+        let nameInput = document.getElementById('nomeCliData');
+        let emailInput = document.getElementById('emailCliData');
+        let foneInput = document.getElementById('foneCliData');
+
+        nameInput.value = '';
+        emailInput.value = '';
+        foneInput.value = '';
+
+        removeLoading();
+    })
 }
 
 document.getElementById('formDataClient').addEventListener('submit', handleSubmit);
+
+
+
+const inputFone = document.getElementById('foneCliData');
+
+inputFone.addEventListener('keypress', (e) => {
+
+    let inputLength = inputFone.value.length;
+
+    if (!checkNum(e)) {
+        e.preventDefault();
+    }
+
+    if (inputLength === 0) {
+        inputFone.value += '(';
+    } else if (inputLength === 3) {
+        inputFone.value += ') '
+    } else if (inputLength === 10) {
+        inputFone.value += '-'
+    }
+
+})
+
+function checkNum(e) {
+    const char = String.fromCharCode(e.keyCode);
+
+    const pattern = '[0-9]'
+
+    if (char.match(pattern)) {
+        return true;
+    }
+}
